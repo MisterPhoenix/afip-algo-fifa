76d5717aace444fc82b0add535dbac84fded67bf
MisterPhoenix <150902093+MisterPhoenix@users.noreply.github.com>
2023-12-17 16:59:44 +0000

    création fonction de suppression

--- functions.py
+++ functions.py
@@ -9,4 +9,14 @@ def load_data(fileName):
           print(ligne.strip())
 
 def suppr_entete(fileName):
-   
+   with open(fileName, 'r') as File:
+      lines = File.readlines()
+
+      ligne_a_suppr = "Position,Team,Games Played,Win,Draw,Loss,Goals For,Goals Against,Goal Difference,Points"
+      ligne_suppr = [line.strip() for line in lines if line.strip() != ligne_a_suppr.strip()]
+
+      with open(fileName, 'w') as File:
+         File.writelines(ligne_suppr)
+
+         for ligne in lines:
+            print(lines)
