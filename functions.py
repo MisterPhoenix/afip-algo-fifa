ff3ebb22503b2882082d84fbba63ad59d9e9619e
MisterPhoenix <150902093+MisterPhoenix@users.noreply.github.com>
2023-12-17 17:00:46 +0000

    commentaires fonctions

--- functions.py
+++ functions.py
@@ -1,5 +1,7 @@
 import os
 
+#fonction récupération des données dans un fichier texte
+
 def load_data(fileName):
     with open(fileName, "r") as File:
         lines = File.readlines()
@@ -7,6 +9,8 @@ def load_data(fileName):
         
         for ligne in lines:
           print(ligne.strip())
+
+#fonction suppression de la première ligne
 
 def suppr_entete(fileName):
    with open(fileName, 'r') as File:
