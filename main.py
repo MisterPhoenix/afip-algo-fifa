773a8f46e93112721556c9a80ffd355556a2929e
MisterPhoenix <150902093+MisterPhoenix@users.noreply.github.com>
2023-12-20 08:22:16 +0000

    mini modif syntaxe

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from functions import *
 fileName = "FIFA_World_Cup/FIFA-2022.txt";
 
 # Charge un fichier en lecture
-file = open(fileName, "r")
+file = open(fileName, "r");
 
 # Charge les données à partir d'un fichier
 data = load_data(fileName);
